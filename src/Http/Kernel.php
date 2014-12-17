5ec35db2cfe98888cf2ab59f244611f81e0825b3
crynobone <crynobone@gmail.com>
2014-12-17 14:32:35 +0800

    Initial commit.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Http/Kernel.php
@@ -0,0 +1,26 @@
+<?php namespace Orchestra\Testing\Http;
+
+use Orchestra\Testbench\Http\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    /**
+     * The application's middleware stack.
+     *
+     * @var array
+     */
+    protected $bootstrappers = [
+    ];
+
+    /**
+     * The application's middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        'Illuminate\Cookie\Middleware\EncryptCookies',
+        'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
+        'Illuminate\Session\Middleware\StartSession',
+        'Illuminate\View\Middleware\ShareErrorsFromSession',
+    ];
+}
