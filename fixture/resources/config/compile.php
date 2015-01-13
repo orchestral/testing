455e0a80e8ff790ab6c6b3f7d4e8bdbe8dacacf1
crynobone <crynobone@gmail.com>
2015-01-13 19:20:41 +0800

    Update to orchestral/platform@e69a762
    Signed-off-by: crynobone <crynobone@gmail.com>

--- fixture/resources/config/compile.php
+++ fixture/resources/config/compile.php
@@ -14,7 +14,22 @@ return [
     */
 
     'files' => [
+        //
+    ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled Config Files
+    |--------------------------------------------------------------------------
+    |
+    | Here you may list config namespace/file which define a "compiles" function
+    | that returns additional files that should be compiled, providing an
+    | easy way to get common files from any packages you are utilizing.
+    |
+    */
+
+    'config' => [
+        //
     ],
 
     /*
