a7e2fee02207f9a1bff37db842fa8c34a8fc8af9
crynobone <crynobone@gmail.com>
2015-07-09 22:50:26 +0800

    Remove disable filters, no longer used.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/TestCase.php
+++ src/TestCase.php
@@ -81,18 +81,6 @@ abstract class TestCase extends TestbenchTestCase
     }
 
     /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application   $app
-     *
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['router']->disableFilters();
-    }
-
-    /**
      * Get base path.
      *
      * @return string
