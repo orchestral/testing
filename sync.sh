010d7d55de31fe576937ebb5e1329932675cc71b
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-25 16:46:01 +0800

    Update language files.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp -rf vendor/orchestra/platform/resources/config/*.php platform/resources/config/
+cp -rf vendor/orchestra/platform/resources/lang/en/*.php platform/resources/lang/en/
